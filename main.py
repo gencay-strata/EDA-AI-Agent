6699fbaee95ce593a993af2177d03faa9f4658ae
gencay-strata <130021280+gencay-strata@users.noreply.github.com>
2024-06-27 16:11:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,7 @@ st.write("Hello, 👋 I am your AI Assistant and I am here to help you with your
 #Explanation sidebar
 with st.sidebar:
     st.write('*Your Data Science Adventure Begins with an CSV File.*')
-    st.caption('''**You may already know that every exciting data science journey starts with a dataset.
-    That's why I'd love for you to upload a CSV file.
-    Once we have your data in hand, we'll dive into understanding it and have some fun exploring it.
-    Then, we'll work together to shape your business challenge into a data science framework.
-    I'll introduce you to the coolest machine learning models, and we'll use them to tackle your problem. Sounds fun right?**
+    st.caption('''**Let's start a Data Science journey, with Stratascratch, shall we?**
     ''')
 
     st.divider()
